e434f00731a0b7f1945dddafc8b2ca94091ad723
ludongxian <gzludx@163.com>
2017-11-03 14:41:39 +0800

    modified:   account-service/pom.xml 	modified:   catalog-service/pom.xml 	modified:   config-service/pom.xml 	modified:   discovery-service/pom.xml 	modified:   edge-service/pom.xml 	modified:   inventory-service/pom.xml 	modified:   online-store-web/pom.xml 	modified:   order-service/pom.xml 	modified:   shopping-cart-service/pom.xml 	modified:   user-service/pom.xml

--- hystrix-dashboard/src/main/java/demo/HystrixApplication.java
+++ hystrix-dashboard/src/main/java/demo/HystrixApplication.java
@@ -3,9 +3,12 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableHystrix
+
 public class HystrixApplication {
 
     public static void main(String[] args) {
