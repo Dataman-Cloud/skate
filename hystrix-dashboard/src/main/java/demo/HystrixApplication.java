c611c8933129a5bdba2a293a136066a00eb2c418
ludongxian <gzludx@163.com>
2017-11-04 15:12:16 +0800

    modify dockerfile

--- hystrix-dashboard/src/main/java/demo/HystrixApplication.java
+++ hystrix-dashboard/src/main/java/demo/HystrixApplication.java
@@ -3,11 +3,9 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableHystrix
 
 public class HystrixApplication {
 
