a7b9c4816f0f70abf44ec1f7a91c9c479ee97da2
ludongxian <gzludx@163.com>
2017-11-14 13:02:11 +0800

    #1修改shell的条件判断

--- skate_stop.sh
+++ skate_stop.sh
@@ -4,31 +4,34 @@ set -e
 p1=$1
 
 if [ ! -n "$p1" ] ;then
-    echo "you have not input a parameter eg. test local or web!"
+    echo "you have not input a parameter. eg. test local or web!"
     exit
 fi
 
-if [ $p1="test" ] ;then
+if [ $p1 = "test" ] ;then
 	echo "stop test demo!"
+	DOCKER_IP=192.168.31.46
 	IMAGE_PREFIX="192.168.31.34/skate"
 	SKATE_VERSION="latest"
 	WEB_IP='192.168.31.46'
-elif [ $p1="local" ] ;then
+elif [ $p1 = "local" ] ;then
 	echo "stop local develop demo!"
+	DOCKER_IP=192.168.31.46
 	IMAGE_PREFIX="skate"
 	SKATE_VERSION="latest"
 	WEB_IP='192.168.31.46'
-elif [ $p1="web" ] ;then
+elif [ $p1 = "web" ] ;then
 	echo "stop public web demo!"
+	DOCKER_IP=10.3.8.23
 	IMAGE_PREFIX="demoregistry.dataman-inc.com/skate"
 	SKATE_VERSION=":latest"
-	WEB_IP='192.168.31.46'
+	WEB_IP='106.75.90.26'
 else
-    echo "you have not input a parameter in  'test'\'local'\'web'"
+    echo "you have not input a parameter. eg. test local or web!"
     exit
 fi
 
-export IMAGE_PREFIX SKATE_VERSION WEB_IP
+export SKATE_VERSION DOCKER_IP WEB_IP IMAGE_PREFIX
 
 #env IMAGE_PREFIX='192.168.31.34/skate' SKATE_VERSION='latest' WEB_IP='192.168.31.46' docker-compose -f docker-compose_test.yml down -v
 docker-compose -f docker-compose_test.yml down -v
