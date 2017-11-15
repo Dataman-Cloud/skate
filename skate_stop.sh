c4e1863e3f3da8c900d064cea075edb400e05879
ludongxian <gzludx@163.com>
2017-11-15 09:50:53 +0800

    #优化处理停止服务的脚本

--- skate_stop.sh
+++ skate_stop.sh
@@ -8,24 +8,24 @@ if [ ! -n "$p1" ] ;then
     exit
 fi
 
+WEB_IP="192.168.31.46"
+
 if [ $p1 = "test" ] ;then
 	echo "stop test demo!"
-	DOCKER_IP=192.168.31.46
+	DOCKER_IP="192.168.31.46"
 	IMAGE_PREFIX="192.168.31.34/skate"
 	SKATE_VERSION="latest"
-	WEB_IP='192.168.31.46'
 elif [ $p1 = "local" ] ;then
 	echo "stop local develop demo!"
-	DOCKER_IP=192.168.31.46
+	DOCKER_IP="192.168.31.46"
 	IMAGE_PREFIX="skate"
 	SKATE_VERSION="latest"
-	WEB_IP='192.168.31.46'
 elif [ $p1 = "web" ] ;then
 	echo "stop public web demo!"
-	DOCKER_IP=10.3.8.23
+	DOCKER_IP="10.3.8.23"
 	IMAGE_PREFIX="demoregistry.dataman-inc.com/skate"
 	SKATE_VERSION=":latest"
-	WEB_IP='106.75.90.26'
+	WEB_IP="106.75.90.26"
 else
     echo "you have not input a parameter. eg. test local or web!"
     exit
@@ -33,5 +33,5 @@ fi
 
 export SKATE_VERSION DOCKER_IP WEB_IP IMAGE_PREFIX
 
-#env IMAGE_PREFIX='192.168.31.34/skate' SKATE_VERSION='latest' WEB_IP='192.168.31.46' docker-compose -f docker-compose_test.yml down -v
+#env IMAGE_PREFIX='192.168.31.34/skate' SKATE_VERSION='latest' DOCKER_IP='192.168.31.46' WEB_IP='192.168.31.46' docker-compose -f docker-compose_test.yml down -v
 docker-compose -f docker-compose.yml down -v
